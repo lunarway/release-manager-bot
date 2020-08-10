fbbbe3682f8fab5d48d0cd86fc1099aecb221c30
Jacob Valdemar <jan@lunar.app>
2020-08-10 10:47:22 +0200

    Add error handling for release manager policy response + edit configuration method (#10)
    * Add Error handling for rel man response
    
    * add body logging if error
    
    * update clientuseragent
    
    * move err handling block and use errors.errorf
    
    * update example config.yml file
    
    * lower case error + defer placement
    
    * edit configuration method

--- issue_release_status.go
+++ issue_release_status.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -83,15 +84,19 @@ func (handler *PRCreateHandler) Handle(ctx context.Context, eventType, deliveryI
 	if err != nil {
 		return errors.Wrap(err, "sending HTTP request")
 	}
-
-	var policyResponse ListPoliciesResponse
+	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return errors.Wrap(err, "reading release-manager HTTP response body")
 	}
 
-	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		logger.Info().Msgf("Request body: %v", body)
+		return errors.Errorf("expected status code 200, but recieved " + fmt.Sprintf("%v", resp.StatusCode))
+	}
+
+	var policyResponse ListPoliciesResponse
 
 	err = json.Unmarshal(body, &policyResponse)
 	if err != nil {
