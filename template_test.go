f2daf42ddec504c39eb3cf4ebae90f51c4631bda
Jacob Valdemar Andreasen <jan@lunar.app>
2020-08-12 12:58:35 +0200

    more code to the test

--- template_test.go
+++ template_test.go
@@ -1,24 +1,74 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBotMessage(t *testing.T) {
 	tt := []struct {
-		name  	string
-		input 	BotMessageData
-		output  string
+		name            string
+		input           BotMessageData
+		expectedMessage string
+		expectedError   error
 	}{
 		{
-			name:	"normal template",
-			input:	BotMessageData{
+			name: "valid template",
+			input: BotMessageData{
+				Template: "'{{.Branch}}' will auto-release to: {{range .AutoReleaseEnvironments}}\n {{.}}{{end}}",
+				Branch:   "master",
+				AutoReleaseEnvironments: []string{
+					"dev",
+				},
+			},
+			expectedMessage: "'master' will auto-release to: \n dev",
+			expectedError:   nil,
+		},
+		{
+			name: "invalid template",
+			input: BotMessageData{
+				Template: "'{{.Branch}}' will auto-release to: {{range .AutoReleaseEnvironments}}\n {{.}}",
+				Branch:   "master",
+				AutoReleaseEnvironments: []string{
+					"dev",
+				},
+			},
+			expectedMessage: "",
+			expectedError:   errors.New(""),
+		},
+		{
+			name: "no template",
+			input: BotMessageData{
 				Template: "",
 				Branch:   "master",
 				AutoReleaseEnvironments: []string{
 					"dev",
 				},
 			},
-			output:	
+			expectedMessage: "",
+			expectedError:   errors.New(""),
 		},
 	}
 
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			var actualMessage string
+			var actualError error
+
+			// Act
+			actualMessage, actualError = BotMessage(tc.input)
+
+			// Assert
+			assert.Equal(t, tc.expectedMessage, actualMessage)
+
+			if tc.expectedError != nil {
+				assert.Error(t, actualError)
+			} else {
+				assert.NoError(t, actualError)
+			}
+		})
+	}
 }
