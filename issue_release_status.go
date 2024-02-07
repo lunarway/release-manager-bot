4a3e23679d375383a62a40ff6d7dedbdf618c1c2
Lunar Renovate <115620512+lunar-renovate@users.noreply.github.com>
2024-02-07 08:25:06 +0000

    Update module github.com/google/go-github/v47 to v58

--- issue_release_status.go
+++ issue_release_status.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v47/github"
+	"github.com/google/go-github/v58/github"
 	"github.com/palantir/go-githubapp/githubapp"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
